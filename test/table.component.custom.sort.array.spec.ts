682ca6bc9f489147a90a1eb5a7b79dcafdd712c8
Landsverk, Andrew <alandsverk@glhec.org>
2018-06-06 14:05:07 -0500

    Adjust the unit test for custom column sorting

--- test/table.component.custom.sort.array.spec.ts
+++ test/table.component.custom.sort.array.spec.ts
@@ -25,10 +25,42 @@ describe('SharkTableComponent', () => {
     fixture.detectChanges();
   }));
 
+  it('should sort', async(() => {
+    const testData = [
+      { col1: [ 2018, 6, 1, 14, 4 ] },
+      { col1: [ 2018, 5, 31, 15, 51 ] },
+      { col1: [ 2018, 5, 31, 15, 52 ] },
+      { col1: [ 2018, 6, 4, 12, 0 ] },
+      { col1: [ 2018, 6, 5, 13, 29 ] }
+    ];
+
+    testData.sort((left, right) => {
+      const leftCol = left['col1'];
+      const rightCol = right['col1'];
+
+      const leftDate = new Date(leftCol[0], leftCol[1] - 1, leftCol[2], leftCol[3], leftCol[4]);
+      const rightDate = new Date(rightCol[0], rightCol[1] - 1, rightCol[2], rightCol[3], rightCol[4]);
+
+      return leftDate.getTime() - rightDate.getTime();
+    });
+
+    expect(testData).toEqual([
+      { col1: [ 2018, 5, 31, 15, 51 ] },
+      { col1: [ 2018, 5, 31, 15, 52 ] },
+      { col1: [ 2018, 6, 1, 14, 4 ] },
+      { col1: [ 2018, 6, 4, 12, 0 ] },
+      { col1: [ 2018, 6, 5, 13, 29 ] }
+    ]);
+
+  }));
+
   it('should sort custom array ASC', async(() => {
     const testPage = [
-      { col1: [ 2016, 7, 27, 7, 12 ] },
-      { col1: [ 2016, 7, 27, 19, 12 ] }
+      { col1: [ 2018, 5, 31, 15, 51 ] },
+      { col1: [ 2018, 5, 31, 15, 52 ] },
+      { col1: [ 2018, 6, 1, 14, 4 ] },
+      { col1: [ 2018, 6, 4, 12, 0 ] },
+      { col1: [ 2018, 6, 5, 13, 29 ] }
     ];
 
     // NONE -> ASC
@@ -55,8 +87,11 @@ describe('SharkTableComponent', () => {
 export class TableTestComponent {
 
   testData = [
-    { col1: [ 2016, 7, 27, 19, 12 ] },
-    { col1: [ 2016, 7, 27, 7, 12 ] }
+    { col1: [ 2018, 6, 1, 14, 4 ] },
+    { col1: [ 2018, 5, 31, 15, 51 ] },
+    { col1: [ 2018, 5, 31, 15, 52 ] },
+    { col1: [ 2018, 6, 4, 12, 0 ] },
+    { col1: [ 2018, 6, 5, 13, 29 ] }
   ];
 
   tableColumns: SharkColumn[] = [
@@ -64,7 +99,7 @@ export class TableTestComponent {
       header: 'Col 1',
       property: 'col1',
       ascendingSortFunction: (left, right) => {
-        return new Date(left[0], left[1], left[2], left[3], left[4]).getTime() - new Date(right[0], right[1], right[2], right[3], right[4]).getTime();
+        return new Date(left[0], left[1] - 1, left[2], left[3], left[4]).getTime() - new Date(right[0], right[1] - 1, right[2], right[3], right[4]).getTime();
       }
     }
   ];
